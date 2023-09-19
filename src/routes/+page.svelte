0dfda6beea6f15f7b67c5df9de83e42534b3de79
Fernando Maclen <fmaclen@gmail.com>
2023-09-19 10:19:11 -0400

    feat: add tauri

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="plate">
+  <h1>Welcome to SvelteKit</h1>
+  <p>Visitz <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+
+<style>
+  .plate {
+    background-color: tomato;
+    border-radius: 25px;
+    max-width: 256px;
+    overflow: hidden;
+  }
+</style>
