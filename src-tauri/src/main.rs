565d7fb950948aad9ed97df0933c6dcee05525cc
Fernando Maclen <fmaclen@gmail.com>
2023-11-18 09:18:30 -0500

    wip: stashed changes from MBP migration

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu};
+use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, };
 use tauri_plugin_positioner::{Position, WindowExt};
 
 fn main() {
     let system_tray_menu = SystemTrayMenu::new();
     tauri::Builder::default()
+        // .enable_macos_default_menu(false) // NOT QUITE
+        .menu(Menu::with_items([
+          MenuEntry::Submenu(Submenu::new(
+            "File",
+            Menu::with_items([
+                MenuItem::CloseWindow.into(),
+                #[cfg(target_os = "macos")]
+                CustomMenuItem::new("hello", "Hello").into(),
+            ]),
+          )),
+        ]));
         .plugin(tauri_plugin_positioner::init())
         .system_tray(SystemTray::new().with_menu(system_tray_menu))
         .on_system_tray_event(|app, event| {
